ae8a9dd6b774642ceba7364cd2702b0a51a0f600
d3npa <d3npa@pm.me>
2021-08-15 19:14:22 -0400

    incorporate changes in library to clean a little

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
-use std::error::Error;
 use tokio_native_tls::TlsStream;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
+
 use url::Url;
 
 use std::io::Write;
+use std::error::Error;
 use std::fs::{self, OpenOptions};
 
 use gemini_hacking as gh;
+use gh::response_codes::*;
 
 type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
@@ -26,21 +28,18 @@ async fn main() -> Result<()> {
 }
 
 async fn handle_request(mut stream: TlsStream<TcpStream>) -> Result<()> {
-    let mut request = Vec::with_capacity(1026);
+    let mut request = Vec::with_capacity(gh::MAX_HEADER_LENGTH);
     stream.read_buf(&mut request).await?;
 
     let request = String::from_utf8(request)?;
-    println!("Request: {:?}", request);
+    eprintln!("[*] Request: {:?}", request);
 
     let url = Url::parse(&request)?;
 
     let response = match url.path() {
-        // show shoutbox and include link to shout
         "/shoutbox" => { render_shoutbox() }, 
-        // shout if no msg or redirect to shoutbox
         "/shout" => { shout(&url) },
-        // redir to /shoutbox
-        _ => { redirect("/shoutbox") } 
+        _ => { redirect_to_shoutbox() },
     };
 
     stream.write_all(&response).await?;
@@ -48,29 +47,31 @@ async fn handle_request(mut stream: TlsStream<TcpStream>) -> Result<()> {
     Ok(())
 }
 
+/// show shoutbox and include link to shout
 fn render_shoutbox() -> Vec<u8> {
     let shoutbox = fs::read_to_string(SHOUTBOX)
-    .expect("[render_shoutbox()] failed to read shoutbox.gmi");
-    gh::create_response(20, Some("text/gemini"), Some(&shoutbox))
+        .expect("in render_shoutbox(): failed to read shoutbox.gmi");
+    gh::create_response(SUCCESS, Some("text/gemini"), Some(&shoutbox))
 }
 
+/// shout if there is a msg or redirect to shoutbox
 fn shout(request: &Url) -> Vec<u8> {
     match request.query() {
-        None => gh::create_response(10, Some("Shout something!"), None),
+        None => gh::create_response(INPUT, Some("Shout something!"), None),
         Some(message) => {
             let mut file = OpenOptions::new()
                 .write(true)
                 .append(true)
                 .open(SHOUTBOX)
                 // BAD. Maybe return a gemini error to client when it fails
-                .expect("failed to open shoutbox file in append mode"); 
+                .expect("in shout(): failed to open shoutbox file"); 
             file.write(format!("{}\n", message).as_bytes())
-                .expect("[shout()] failed to write to shoutbox file");
-            gh::create_response(31, Some("/shoutbox"), None)
+                .expect("in shout(): failed to write to shoutbox file");
+            gh::create_response(SUCCESS, Some("/shoutbox"), None)
         }
     }
 }
 
-fn redirect(location: &str) -> Vec<u8> {
-    gh::create_response(31, Some(location), None)
+fn redirect_to_shoutbox() -> Vec<u8> {
+    gh::create_response(REDIRECT_PERMANENT, Some("/shoutbox"), None)
 }
